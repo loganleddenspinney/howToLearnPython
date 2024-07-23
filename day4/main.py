0e82219a349a92eb8a17ccbb7908a18ae814d665
Logan Ledden Spinney <loganleddenspinney@gmail.com>
2024-07-23 12:30:09 -0400

    day 4 commit

---
+++ day4/main.py
@@ -0,0 +1,8 @@
+import my_module
+
+#random_integer = random.randint(1,10)
+
+print(my_module.pi)
+
+#this file allows me to run code from this file but pull code from another file using the module feature by importing the name of file as shown at the top "import my_module" then
+#to pull the information you print(name of the file then . whatever you want)
---
+++ day4/randomtest.py
@@ -0,0 +1,13 @@
+import random
+
+#using random numbers or generated things makes it capable to do games like rock paper scisssors 
+
+random_integer = random.randint(1,10)
+
+print (random_integer)
+
+random_float = random.random()
+print(random_float)
+
+love_score = random.randint(1, 100)
+print(f"your love score is {love_score}")
