186055f7da03031d0d6a3d30d0a936178e2c1586
Logan Ledden Spinney <loganleddenspinney@gmail.com>
2024-07-27 13:55:34 -0400

    update

--- day6/reborgsworld.py
+++ day6/reborgsworld.py
@@ -19,4 +19,26 @@ for i in range(6):
     
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
 
+# while loop (this while does the same as the for loop above)
 
+number_of_hurdles = 6
+
+while number_of_hurdles > 0:
+    morg()
+    number_of_hurdles -= 1
+    
+# for 
+#for item in list_of_items:
+#   do something to each item
+#
+#for number in range(a,b):
+#   print(number)
+
+#while
+#
+#while something_is_true:
+#   do somthing repeatedly
+
+# while not at_goal():
+#    morg()
+    
