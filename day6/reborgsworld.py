1dd8f45e110e0e2e6fe0c645c3fc5a39f8a234ac
Logan Ledden Spinney <loganleddenspinney@gmail.com>
2024-08-05 15:38:26 -0400

    finish day 6

--- day6/reborgsworld.py
+++ day6/reborgsworld.py
@@ -42,3 +42,119 @@ while number_of_hurdles > 0:
 # while not at_goal():
 #    morg()
     
+    
+    """
+    This is an example on how to use a while in a while with if statements in each while
+    
+    heres how to do reborgs world hurdle 3 
+    https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+   
+    def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+
+while wall_in_front():
+    jump()
+    if at_goal():
+        break
+    while front_is_clear():
+        move()
+        if at_goal():
+            break
+
+    
+    
+    
+    
+    """
+    
+    """
+    The teacher solved as:
+    
+    while not at_goal():
+        if wall_in_front():
+            jump()
+        else:
+            move()
+    
+    
+    """
+    
+    
+    
+    
+    """
+    
+    https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+    rebord hurdle 4
+    
+    
+    def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+        
+        
+
+while not at_goal():
+        if wall_in_front():
+            jump()
+        else:
+            move()
+    
+    
+    
+    
+    """
+    
+"""
+Reborgs world maze 
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while front_is_clear():
+    move()
+turn_left()
+        
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif right_is_clear():
+            turn_right() and move()
+    else:
+        turn_left()
+
+
+
+
+
+
+
+"""
