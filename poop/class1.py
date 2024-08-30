e8960a0e88a539744f0e9393cf2adf1178fe89f8
Logan Ledden Spinney <loganleddenspinney@gmail.com>
2024-08-30 16:58:02 -0400

    understanding python format of poop in leetcode

--- poop/class1.py
+++ poop/class1.py
@@ -1,10 +1,25 @@
-class Solution():
+class Solution:
     
-    def runningSum(self, nums):
+    def __init__(self, nums):     
         self.nums = nums
-        nums = []
-        for i in range(1,len(nums)):
-            nums[i] += nums[i-1]
-        print(nums)   
         
-    
+          
+    def runningSum(self, nums):
+        a = 0
+        lst = []
+        for i in nums:
+            a = i + a
+            lst.append(a)
+        print(lst)
+        return(lst)
+        
+
+
+# the following lines of code are how to get the input and output
+nums = [1,3,4,5,6]
+answer = Solution(nums)
+answer.runningSum(nums)        
+
+# this problem is leetcode 1480 and this is how it is solved on vscode 
+# however leetcode already has the init and does not need that it only needs the function 
+# in leetcode you need a return or it wont work even tho if i want to see it i need to print it so I did both to make more sense to me.
--- poop/class1_input.py
+++ poop/class1_input.py
@@ -1,5 +1,11 @@
 from class1 import Solution
 
-nums = [1,1,1,1,1]
+nums = [1,3,4,5,6]
+answer = Solution(nums)
 
-nums.runningSum()
+answer.runningSum(nums)
+
+
+# this is the input and output of leetcode problem 1480 
+
+# this is just to practice pulling the code from the method using from (file) import (object)
